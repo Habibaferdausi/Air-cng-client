3084037bcf673761a4c61f1d5fa16ec3b6c2740f
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-06-03 15:52:31 +0600

    add nav

--- src/Components/Shared/Nav/Nav.jsx
+++ src/Components/Shared/Nav/Nav.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Logo from "./Logo";
 import Container from "./Container";
 import Search from "./Search";
+import MenuDropdown from "./MenuDropdown";
 
 const Nav = () => {
   return (
@@ -11,7 +12,7 @@ const Nav = () => {
           <div className="flex flex-row  items-center justify-between gap-3 md:gap-0">
             <Logo />
             <Search />
-            {/* <MenuDropdown /> */}
+            <MenuDropdown></MenuDropdown>
           </div>
         </Container>
       </div>
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyABhSioD-GdIe65snplLH7ij07XXn6yUFA",
+  authDomain: "booking-c44eb.firebaseapp.com",
+  projectId: "booking-c44eb",
+  storageBucket: "booking-c44eb.appspot.com",
+  messagingSenderId: "251645286761",
+  appId: "1:251645286761:web:14c005ef6d3a4d41da45f8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
